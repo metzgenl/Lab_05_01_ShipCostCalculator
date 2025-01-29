cf5b388f114d6a67c447aff24cfcc498257ffb02
Nate Metzger <metzgenl@mail.uc.edu>
2025-01-28 19:59:00 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//Import Scanner
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        ShipCostCalculator shipCost = new ShipCostCalculator();
+
+        shipCost.getShippingCost();
+    }
+}
---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,37 @@
+//Import Scanner
+import java.util.Scanner;
+
+public class ShipCostCalculator {
+    double itemPrice = 0;
+    double shippingCost = 0;
+    double shippingTax = 0.02;
+    double totalPrice = 0;
+    String incorrectInput = "";
+
+    public void getShippingCost(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the item Price $");
+
+        //Check for valid input from user
+        if (in.hasNextDouble()){
+            itemPrice = in.nextDouble();
+            in.nextLine();
+
+            // Calculate the Shipping Price and Total Price
+            if (itemPrice >= 100){
+                totalPrice = itemPrice;
+            }
+            else{
+                shippingCost = itemPrice * shippingTax;
+                totalPrice = itemPrice + shippingCost;
+            }
+            System.out.println("The shipping cost for the item is $" + shippingCost + " and the total price is $" + totalPrice + ".");
+        }
+        else{
+            incorrectInput = in.nextLine();
+            System.out.println("\n" + incorrectInput + " is not a valid number. Run the program again and enter a valid amount!");
+        }
+
+
+    }
+}
